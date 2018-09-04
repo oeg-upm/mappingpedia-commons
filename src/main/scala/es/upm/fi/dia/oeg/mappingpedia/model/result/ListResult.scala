98e4528857d3b5d24a01833f6a1c645e031ec4d8
Freddy Priyatna <freddy.priyatna@gmail.com>
2018-09-04 14:28:41 +0200

    add status code and message in ListResult

--- src/main/scala/es/upm/fi/dia/oeg/mappingpedia/model/result/ListResult.scala
+++ src/main/scala/es/upm/fi/dia/oeg/mappingpedia/model/result/ListResult.scala
@@ -6,8 +6,19 @@ import scala.collection.JavaConverters._
   * Created by fpriyatna on 04/04/2017.
   */
 class ListResult [A] (val count:Integer, val results:Iterable[A]) {
+  var statusCode = 200;
+  var statusMessage = "OK";
+
   def this(results:Iterable[A]) = {
     this(results.size, results)
+  }
+
+  def this(results:java.util.Collection[A]) = {
+    this(results.size, results.asScala)
+  }
+
+  def this() = {
+    this(Nil)
   }
 
   def getCount() = this.count;
@@ -15,4 +26,7 @@ class ListResult [A] (val count:Integer, val results:Iterable[A]) {
   def getResults() = this.results.asJava;
 
   override def toString = s"ListResult($getCount, $getResults)"
+
+  def getStatus_code = this.statusCode
+  def getStatusMessage = this.statusMessage
 }
